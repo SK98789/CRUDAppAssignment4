cedb5a73b91ec6aab4377057ed4e0d517ecf1ece
sk98789 <s_korzekwa@uncg.edu>
2024-05-28 13:47:43 -0400

    Creating Repository

---
+++ src/main/java/com/CSC340/CRUDAppAssignment4/CrudAppAssignment4Application.java
@@ -0,0 +1,13 @@
+package com.CSC340.CRUDAppAssignment4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAppAssignment4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudAppAssignment4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/CSC340/CRUDAppAssignment4/CrudAppAssignment4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CSC340.CRUDAppAssignment4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudAppAssignment4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

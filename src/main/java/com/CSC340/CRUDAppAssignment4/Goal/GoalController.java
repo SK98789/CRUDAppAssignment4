a7521439af4072c872ea54e39adf7aea62ba153a
sk98789 <s_korzekwa@uncg.edu>
2024-05-29 21:06:15 -0400

    Added content to HTMl and added post mapping for creating a new goal

--- src/main/java/com/CSC340/CRUDAppAssignment4/Goal/GoalController.java
+++ src/main/java/com/CSC340/CRUDAppAssignment4/Goal/GoalController.java
@@ -1,4 +1,20 @@
 package com.CSC340.CRUDAppAssignment4.Goal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/goals")
 public class GoalController {
+    @Autowired
+    GoalService goalService;
+
+    @PostMapping("/create")
+    public String addNewGoal(Goal goal){
+        goalService.addNewGoal(goal);
+        return "redirect:/goals/all";
+    }
+
 }

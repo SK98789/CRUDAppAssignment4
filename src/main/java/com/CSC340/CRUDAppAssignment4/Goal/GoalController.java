0ca1a24960d231417b485e75371d9e8a75ad173b
sk98789 <s_korzekwa@uncg.edu>
2024-05-30 00:56:02 -0400

    Added more mapping

--- src/main/java/com/CSC340/CRUDAppAssignment4/Goal/GoalController.java
+++ src/main/java/com/CSC340/CRUDAppAssignment4/Goal/GoalController.java
@@ -2,6 +2,9 @@ package com.CSC340.CRUDAppAssignment4.Goal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,5 +19,22 @@ public class GoalController {
         goalService.addNewGoal(goal);
         return "redirect:/goals/all";
     }
+
+    @GetMapping("/all")
+    public String getAllStudents(Model model) {
+        model.addAttribute("goalList", goalService.getAllGoals());
+        return "Goals";
+    }
+
+    @GetMapping("/newGoal")
+    public String getCreationForm(Model model){
+        return "CreateAGoal";
+    }
+    @GetMapping("/{id}")
+    public String getGoalById(@PathVariable int id, Model model) {
+        model.addAttribute("goal", goalService.getGoalById(id));
+        return "GoalDetails";
+    }
+
 
 }
